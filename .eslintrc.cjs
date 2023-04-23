347c79bb6155e9912abf714670d7ad0e524f2344
Rifky Setiadji <rifkysetiadjie@gmail.com>
2023-04-23 19:44:56 +0700

    First init

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/triple-slash-reference': 0,
+    'import/no-absolute-path': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
